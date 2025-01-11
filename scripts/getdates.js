ce22df7f8082a2616f7f40725ca9a75ec23d2fbc
Misael-Casjor <mcastro2@byupathway.edu>
2025-01-11 18:22:07 -0500

    last javascrip changes

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,3 +1,7 @@
 const currentYear = document.querySelector("#currentYear");
 const today = new Date();
 currentYear.innerHTML = `© <span class="highlight">${today.getFullYear()}</span> Misael Castro`;
+
+const lastModified = document.querySelector("#lastModified")
+let nLastModif = new Date(document.lastModified);
+lastModified.innerHTML = `Last Modified: <span class="highlight">${nLastModif}</span>`
