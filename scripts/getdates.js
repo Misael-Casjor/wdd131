ca08fdfd55ecf1ff0b4c31aaa0af557184aa8a72
Misael-Casjor <mcastro2@byupathway.edu>
2025-09-08 18:14:49 -0400

    hover added to nav bar, padding and margin issues solved at header

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,3 +1,7 @@
 const dates = document.querySelector("#currentyear")
+const lastChange = document.querySelector("#lastModified")
 const today = new Date()
 dates.innerHTML = `© <span class="highlight">${today.getFullYear()}</span> Misael Castro`;
+
+let oLastModif = new Date(document.lastModified);
+lastChange.innerHTML = `Last Modified: <span class="highlight">${oLastModif}</span>`
